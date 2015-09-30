f85271d171334170df552866cd13fda0a1a74d5e
Kyle McAlpine <k@kylejm.io>
2015-09-30 22:13:03 +0100

    Initial Commit

---
+++ JimTests/JimTests.swift
@@ -0,0 +1,36 @@
+//
+//  JimTests.swift
+//  JimTests
+//
+//  Created by Kyle McAlpine on 30/09/2015.
+//  Copyright © 2015 Kyle McAlpine. All rights reserved.
+//
+
+import XCTest
+@testable import Jim
+
+class JimTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
