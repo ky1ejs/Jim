34e5c7c6e99a3ab7f5dc49d9e16549b3ad47ca61
Kyle McAlpine <k@kylejm.io>
2015-09-30 22:29:41 +0100

    Parse added

--- Jim/ViewController.swift
+++ Jim/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import ParseUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.presentViewController(PFLogInViewController(), animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
